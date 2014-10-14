d63f5ce9f35e9b9c857e29ecf4a78f966999a974
Dominik Vesely <vesely@dominoo.cz>
2014-10-14 10:36:16 +0200

    modal ve storyboardu

--- bi-ios-2014-04-controllers/YellowViewController.m
+++ bi-ios-2014-04-controllers/YellowViewController.m
@@ -1,36 +1,42 @@
 //
-//  ViewController.m
+//  YellowViewController.m
 //  bi-ios-2014-04-controllers
 //
 //  Created by Dominik Vesely on 14/10/14.
 //  Copyright (c) 2014 Ackee s.r.o. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "YellowViewController.h"
 
-@interface ViewController ()
+@interface YellowViewController ()
 
 @end
 
-@implementation ViewController
+@implementation YellowViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.tabBarItem.title = @"Red";
-    // Do any additional setup after loading the view, typically from a nib.
+    //self.view.backgroundColor = [UIColor yellowColor];
+    self.title = @"Yellow";
+    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)closeAction:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    NSLog(@"prepareSegue");
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
 }
-
+*/
 
 @end
